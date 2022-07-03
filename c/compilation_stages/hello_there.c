8af955c57d9569cfb7daf956c375ff82e2468a30
Vladimir Nikulin <mail.jorey@gmail.com>
2022-07-04 01:44:47 +0300

    Добавлены c и cpp файлы на различных стадиях компиляции

---
+++ c/compilation_stages/hello_there.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define RETURN return 0
+
+int main()
+{
+    printf("Hello there!\n");
+    printf("There is no escape!\n");
+    RETURN;
+}
---
+++ cpp/compilation_stages/hello_there.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define RETURN return 0
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello there!" << endl;
+    cout << "There is no escape!" << endl;
+    RETURN;
+}
