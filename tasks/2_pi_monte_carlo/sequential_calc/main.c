c1933e8d06d6e56360cda178d20c53b02369f159
Vladimir Nikulin <mail.jorey@gmail.com>
2022-08-04 16:45:26 +0300

    Пи Монте Карло на алгоритме XorShift1024

--- tasks/2_pi_monte_carlo/sequential_calc/main.c
+++ tasks/2_pi_monte_carlo/sequential_calc/main.c
@@ -6,47 +6,45 @@
 #include <math.h>
 #include "xor_shift.h"
 
-#define EPS 0.0001f // точность, c которой нужно найти число Пи
+#define EPS 0.0001 // точность, c которой нужно найти число Пи
 
 int main()
 {
-    unsigned long m_time = time(NULL);
-    seed_1024(m_time);
+    seed_1024(time(NULL)); // сид для генератора случайных чисел
       
-    unsigned long long gp = 0, ap = 0;
+    unsigned long long gp = 0, ap = 0; // хорошие точки и всего точек, соответственно
 
-    float e = -1;
-    long double x = 0, y = 0, len = 0;
-    long double prev_pi = -1, curr_pi = 0;
+    long double x = 0, y = 0, len = 0; // координаты точки и её расстояние до центра окружности
+    long double prev_pi = -1, curr_pi = 0, e = 1; // предыдущее Пи, текущее Пи и их разность
 
     // считаем пи, пока не добъёмся нужной точности
     do
     {
-        ap++; // всего точек, для всех Пи одинаково
+        ap++; // считаем всего точек 
 
-        // пи
+        // сгенерируем координаты точки
         x = (long double) next_1024() / (long double) XOR_SHIFT_RAND_MAX;
         y = (long double) next_1024() / (long double) XOR_SHIFT_RAND_MAX;
            
-        len = x*x + y*y; 
+        len = x*x + y*y; // расстояние до центра окружности
 
         // точка входит в круг
-        if (len <= 1.0f) gp++;
+        if (len <= 1.0f) gp++; // хорошие точки
         
-        if (ap % 1000000 == 0)
+        if (ap % 1000 == 0) // раз в 1000 точек сравнить значения Пи, чтобы проверить удовлетворение точности EPS
         {            
             prev_pi = curr_pi;
 
-            curr_pi = 4.0f * (long double) gp / (long double) ap;
-            curr_pi = ((int) (curr_pi / EPS)) * EPS; // отбрасываем лишнюю дробную часть
+            curr_pi = 4.0f * (long double) gp / (long double) ap; // вычисляем Пи
             
-            e = fabs(curr_pi - prev_pi);
-            e = ((int) (e / EPS)) * EPS;
-            printf("prev_pi = %Lf20; curr_pi = %Lf; eps = %f; gp = %lu; ap = %lu\n", prev_pi, curr_pi, e, gp, ap);
+            e = fabs(curr_pi - prev_pi); // разница между прошлым Пи и текущим Пи (точность)
+                                         
+            // вывод информации с итерации
+            printf("prev_pi = %Lf20; curr_pi = %Lf; eps = %f; gp = %lu; ap = %lu\n", prev_pi, curr_pi, (double) e, gp, ap);
         }
     } 
-    while (e != EPS);
+    while (e > EPS); // как только разница между прошлым Пи и текущим Пи будет > EPS, выйти из цикла
 
-    printf("PI = %Lf;\n", curr_pi);
+    printf("PI = %Lf;\n", curr_pi); // вывод рассчитанного Пи
     return 0;
 }
