e8ac19bf73df9616a9e21d62596658a6cf4b4e49
Vladimir Nikulin <mail.jorey@gmail.com>
2022-08-16 00:47:54 +0300

    Добавлены ключи (обработка аргументов функции main) в задаче Пи Монте Карло

--- cpp/2_pi_monte_carlo/main.cpp
+++ cpp/2_pi_monte_carlo/main.cpp
@@ -4,16 +4,52 @@
 #include "stdafx.hpp"
 #include "true_pi_selector_class.hpp"
 
-#define EPS 1e-4 // точность, c которой нужно найти число Пи
+#include <sstream>
 
-int main()
+#define HELP_INDENT 4
+
+int main(int argc, char *argv[])
 {
-    // Объект класса TruePISelector для вычисления Пи
-    TruePISelector selector(5, EPS);
-    double result = selector.select_pi();
+    if ( argc == 2 )
+    {
+        if ( std::string(argv[1]) == "-v" || std::string(argv[1]) == "--version" )
+        {
+            std::cout << "1.0.0.0" << std::endl;
+        }
+        else if ( std::string(argv[1]) == "-h" || std::string(argv[1]) == "--help" )
+        {
+            std::cout << "This is a Monte Carlo search engine for a PI number. Usage:\n"
+                << std::string(HELP_INDENT, ' ') << argv[0] <<" [options]\n"
+                << std::string(HELP_INDENT, ' ') << argv[0] << " eps_value\n"
+                << "\nOptions:\n"
+                << std::string(HELP_INDENT, ' ') << "-h, --help" << "\t\t\tShow program help info" << "\n"
+                << std::string(HELP_INDENT, ' ') << "-v, --version" << "\t\tDisplay program version" << "\n"
+                << std::endl;
+        }
+        else 
+        {
+            // в этом случае там просто значение EPS
+            char *endptr = 0;
+            strtod(argv[1], &endptr);
 
-    // вывод рассчитанного Пи
-    std::cout << "\nPI = " << result << std::endl;
+            if ( *endptr != '\0' || endptr == argv[1] )
+            {
+                // bad double
+                std::cout << "EPS is not a float number: " << argv[1] << std::endl;
+                return EXIT_FAILURE;
+            }
+            
+            // good double
+            double eps = std::strtod(argv[1], NULL);
 
-    return 0;
+            // Объект класса TruePISelector для вычисления Пи
+            TruePISelector selector(5, eps);
+            double result = selector.select_pi();
+
+            // вывод рассчитанного Пи
+            std::cout << "\nPI = " << result << std::endl;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
